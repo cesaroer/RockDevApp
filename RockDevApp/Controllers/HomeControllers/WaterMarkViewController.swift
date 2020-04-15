a41772a2f73e2c6e401dfb5cfe43a2d71ebb36b8
CesarVargas <cesarvarcard@gmail.com>
2020-04-15 00:10:58 -0500

    Se añade vista para hacer pruebas
    Se añade vista para hacer pruebas y
    se añade la opcion para acceder a ella en el menú
    latera.

---
+++ RockDevApp/Controllers/HomeControllers/WaterMarkViewController.swift
@@ -0,0 +1,11 @@
+//
+//  WaterMarkViewController.swift
+//  RockDevApp
+//
+//  Created by Cesar on 14/04/20.
+//  Copyright © 2020 CesarVargas. All rights reserved.
+//
+
+import UIKit
+
+class WaterMarkViewController: UIViewController 
